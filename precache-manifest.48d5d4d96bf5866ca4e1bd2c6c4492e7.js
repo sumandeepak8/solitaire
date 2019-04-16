6b39737a36b7bc6178c5c216167d02748dc6671f
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-04-16 17:32:22 +0530

    Updates

---
+++ precache-manifest.48d5d4d96bf5866ca4e1bd2c6c4492e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "173ac1323f551c1f1b64",
+    "url": "/static/js/main.3a076114.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "173ac1323f551c1f1b64",
+    "url": "/static/css/main.e06f5a64.chunk.css"
+  },
+  {
+    "revision": "2b6e4efd48844884b447cdbcc545d127",
+    "url": "/index.html"
+  }
+];
